3e58a9b3ad500fa1b7b8319c2d291246a774e9e2
Christian Schmied <cs8898@gmx.de>
2017-09-12 22:09:05 +0200

    added basic Slack Integration

---
+++ src/main/java/tools/devnull/jenkins/plugins/buildnotifications/SlackNotifier.java
@@ -0,0 +1,104 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2016-2017 Marcelo "Ataxexe" Guimarães
+ * <ataxexe@devnull.tools>
+ *
+ * ----------------------------------------------------------------------
+ * Permission  is hereby granted, free of charge, to any person obtaining
+ * a  copy  of  this  software  and  associated  documentation files (the
+ * "Software"),  to  deal  in the Software without restriction, including
+ * without  limitation  the  rights to use, copy, modify, merge, publish,
+ * distribute,  sublicense,  and/or  sell  copies of the Software, and to
+ * permit  persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The  above  copyright  notice  and  this  permission  notice  shall be
+ * included  in  all  copies  or  substantial  portions  of the Software.
+ *                        -----------------------
+ * THE  SOFTWARE  IS  PROVIDED  "AS  IS",  WITHOUT  WARRANTY OF ANY KIND,
+ * EXPRESS  OR  IMPLIED,  INCLUDING  BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN  NO  EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM,  DAMAGES  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT  OR  OTHERWISE,  ARISING  FROM,  OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE   OR   THE   USE   OR   OTHER   DEALINGS  IN  THE  SOFTWARE.
+ */
+package tools.devnull.jenkins.plugins.buildnotifications;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Publisher;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * A notifier that uses Slack to delivery messages
+ *
+ * @author cs8898
+ */
+public class SlackNotifier extends BaseNotifier {
+
+  /**
+   * @see BaseNotifier#BaseNotifier(String, String, String, String, String, boolean, String)
+   */
+  @DataBoundConstructor
+  public SlackNotifier(String globalTarget,
+                          String successfulTarget,
+                          String brokenTarget,
+                          String stillBrokenTarget,
+                          String fixedTarget,
+                          boolean sendIfSuccess,
+                          String extraMessage) {
+    super(globalTarget, successfulTarget, brokenTarget, stillBrokenTarget, fixedTarget, sendIfSuccess, extraMessage);
+  }
+
+  @Override
+  protected Message createMessage(String target, AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+    SlackDescriptor descriptor = (SlackDescriptor) getDescriptor();
+    return new SlackMessage(descriptor.getBotToken(), target, replaceEnvString(build, getExtraMessage()));
+  }
+
+  /**
+   * The descriptor for the SlackNotifier plugin
+   */
+  @Extension
+  public static class SlackDescriptor extends BuildStepDescriptor<Publisher> {
+
+    private String botToken;
+
+    public SlackDescriptor() {
+      load();
+    }
+
+    public String getBotToken() {
+      return botToken;
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+      JSONObject config = json.getJSONObject("slack");
+      this.botToken = config.getString("botToken");
+      save();
+      return true;
+    }
+
+    @Override
+    public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+      return true;
+    }
+
+    @Override
+    public String getDisplayName() {
+      return "Slack Notification";
+    }
+
+  }
+
+}
+
